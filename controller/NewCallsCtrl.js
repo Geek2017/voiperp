98a8607084568d7f346b27bfefd04e2bcbf6087d
xdxdiagx <cj.lignes@gmail.com>
2020-11-18 17:32:21 +0800

    Calls

--- controller/NewCallsCtrl.js
+++ controller/NewCallsCtrl.js
@@ -4,6 +4,25 @@ angular.module('fvs').controller('NewCallsCtrl', function ($scope) {
 
     var count = 0;
 
+    $("#call").click(function() {
+      $.ajax({
+        url: "make_call.php",
+        method: "POST",
+        data: {
+            number: "+639565776616",
+            // loader: loader,
+            // folder_name: folder_name,
+            // action: action,
+            // copy: "copy"
+        },
+        success: function(data) {
+          console.log(data);
+          console.log("Calling");
+
+        }
+    });
+  });
+
 $(".digit").on('click', function() {
   var num = ($(this).clone().children().remove().end().text());
   if (count < 11) {
